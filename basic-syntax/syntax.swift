ba9a7906430fe05f0a67bd64c37dd437f3bdb6db
Mushfequr-Rahman <42585345+Mushfequr-Rahman@users.noreply.github.com>
2018-12-05 05:00:04 -0500

    Create syntax.swift

---
+++ basic-syntax/syntax.swift
@@ -0,0 +1,30 @@
+// Much Simpler than the original print statements found
+// in C++
+
+print("Final Project by Mohammad Mushfequr Rahman Student ID 100659376")
+
+//Variable declaration is also much simpler as you can
+// see:
+var My_var=46
+
+/*The compiler determines what type of variable we are using
+at run time,
+ If we have to initlaze a variable with out giving an
+ intial type, we can do by casting explicitlu
+ */
+var my_float: Double = 4
+print(my_float)  // => 4.0
+
+/*
+We can also create top level contants similar
+to def in clojure by using let
+*/
+
+let My_constant = 90
+
+var sum = 0 // Similar to C we have to intialize varibales
+for number in My_List
+{
+  sum += number
+}
+print(sum) //=>26
